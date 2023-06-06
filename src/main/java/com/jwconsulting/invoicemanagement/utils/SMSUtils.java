4044fddb0f65ce94b7e00a8e5dbd2382695d0227
jakewilson13 <jwilsonlax13@gmail.com>
2023-06-05 20:18:47 -0400

    changes to utils

--- src/main/java/com/jwconsulting/invoicemanagement/utils/SMSUtils.java
+++ src/main/java/com/jwconsulting/invoicemanagement/utils/SMSUtils.java
@@ -9,7 +9,7 @@ import static com.twilio.rest.api.v2010.account.Message.creator;
 public class SMSUtils {
     public static final String FROM_NUMBER = "+18669513570";
     public static final String SID_KEY = "AC8098c5fae31cbb45b9fa9d80c8b26e18";
-    public static final String TOKEN_KEY = "23b5c623cd5159a57374629c3397ddd7";
+    public static final String TOKEN_KEY = "";
     public static void sendSMS(String to, String messageBody) {
         Twilio.init(SID_KEY, TOKEN_KEY);
         Message message = creator(new PhoneNumber("+" + to), new PhoneNumber(FROM_NUMBER), messageBody).create();
